8b7100d52d1af12871c49dadd9d2f8d67f55fa63
AlexDev404 <alexdev404@users.noreply.github.com>
2022-12-19 17:35:41 -0600

    [TODO] Jump beyond the 512K barrier

--- src/main.rs
+++ src/main.rs
@@ -10,31 +10,49 @@ fn panic(_info: &PanicInfo) -> ! {
     }
 }
 
+fn fill_screen(vga: *mut u8, screen_x: isize, screen_y: isize, color: u8) {
+    for x in 0..screen_x {
+        for y in 0..screen_y {
+            unsafe {
+                *vga.offset(x + y * 320) = color;
+            }
+        }
+    }
+}
+
 // static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 // #[link_section = ".text.init"]
 pub unsafe extern "C" fn _start() -> ! {
-    unsafe {
-        let vga = 0xa0000 as *mut u8;
+    // Pixel FMT: x+y*screen_x
+    // Setting the unit of the memory to units
+    let vga = 0xa0000 as *mut u8;
 
-        // Draw a horizonal line from the center of the screen
-        for x in 0..100 {
-            *vga.offset(320 / 2 + x + 200 / 2 * 320) = 0x0a; // Should display a pixel in the center of the screen
+    for _h in 0..8 {
+        // Fill the screen (rainbow)
+        for i in 0..0xff {
+            fill_screen(vga, 320, 200, i);
+            // Beyond the 512K barrier we crash
         }
-
-        // Draw a vertical line from the center of the screen
-        // for y in 0..100 {
-            *vga.offset(320 / 2 + 200 / 2 + y * 320) = 0x0a; // Should display a pixel in the center of the screen
-        // }
-        
-        // Draw a diagonal line from the center of the screen
-        for xy in 0..100 {
-            *vga.offset(320 / 2 + xy + 200 / 2 + xy * 320) = 0x0a; // Should display a pixel in the center of the screen
-        }
-
-        // END
     }
+
+    // Draw a horizonal line from the center of the screen
+    // for x in 0..100 {
+    //     *vga.offset(320 / 2 + x + 200 / 2 * 320) = 0x0a;
+    // }
+
+    // Draw a vertical line from the center of the screen
+    // for y in 0..100 {
+    // *vga.offset(320 / 2 + 200 / 2 + y * 320) = 0x0a;
+    // }
+
+    // Draw a diagonal line from the center of the screen
+    // for xy in 0..100 {
+    // *vga.offset(320 / 2 + xy + 200 / 2 + xy * 320) = 0x0a;
+    // }
+
+    // END
 
     // loop {
     // }
