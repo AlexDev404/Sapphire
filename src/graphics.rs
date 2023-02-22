6821b5591f3bb29af2bc9b45c64e81d18c0803af
AlexDev404 <alexdev404@users.noreply.github.com>
2023-02-22 00:05:18 -0600

    @audit-issue Fix the PITCH and BPP

--- src/graphics.rs
+++ src/graphics.rs
@@ -26,9 +26,9 @@ pub fn fill_screen(vga: *mut u8, screen_x: isize, screen_y: isize, color: u8) {
     for x in 0..screen_x {
         for y in 0..screen_y {
             unsafe {
-                // let offset = y * *PITCH + x * *BPP;
-                let offset = x + y * 640; // @audit-issue Hardcode - Bad
-                *vga.offset(offset) = color;
+                let offset = (y as u32) * *PITCH + (x as u32) * *BPP; // framebuffer_base + (y * bytes_per_scanline) + (x * bytes_per_pixel)
+                // let offset = x + y * 640; // @audit-issue Hardcode - Bad
+                *vga.offset(offset as isize) = color;
             }
         }
     }
@@ -42,11 +42,11 @@ pub fn putpixel(vga: *mut u8, color: u8, x: isize, y: isize) {
         // let bpp = b as isize;
 
         // @audit-issue @todo NEEDS FIXING!!!
-        // Actual Pixel FMT = framebuffer + y * bytes_per_scan_line + x * bytes_per_pixel
-        // let offset = y as u32 * *PITCH + x as u32 * *BPP;
+        // Actual Pixel FMT = framebuffer + (y * bytes_per_scan_line) + (x * bytes_per_pixel)
+        // let offset = (y as u32 * *PITCH) + (x as u32 * *BPP); // framebuffer_base + (y * bytes_per_scanline) + (x * bytes_per_pixel)
 
         // @audit PATCH
-        // TEMP Pixel FMT: x+y*screen_x
+        // TEMP Pixel FMT: x + y * screen_x
         let offset = x + y * 640; // @audit-issue Hardcode - Bad
         *vga.offset(offset as isize) = color;
     }
