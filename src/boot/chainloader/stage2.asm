d6de8ae1f6fd9ea0fa4389b234e8ff1c02b699d0
AlexDev404 <alexdev404@users.noreply.github.com>
2024-08-23 03:40:02 -0600

    Font renders properly to the screen now

--- src/boot/chainloader/stage2.asm
+++ src/boot/chainloader/stage2.asm
@@ -85,9 +85,9 @@ PModeMain:
 
 	; mov ax, 0x0F ; Pixel Color. We chose red
 	; @audit-issue PITCH + BPP Reassignment
-	mov esi, DWORD [vbe_mode_block.pitch] ; The pitch is at offset 12h in the mode information block
-	mov ebx, DWORD [vbe_mode_block.framebuffer]; Our framebuffer
-	mov edi, DWORD [vbe_mode_block.bpp]; Our BPP
+	; mov esi, DWORD [vbe_mode_block.pitch] ; The pitch is at offset 12h in the mode information block
+	; mov ebx, DWORD [vbe_mode_block.framebuffer]; Our framebuffer
+	; mov edi, DWORD [vbe_mode_block.bpp]; Our BPP
 	; ; add ebx, 180050; pixel_offset = y * pitch + ( x * ( bpp / 8 )) + framebuffer;
 	; ; mov [ebx], ax
 
--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,9 @@ static F_HEIGHT: isize = 16; // Rows
 static F_WIDTH: isize = 8; // Columns
 
 pub fn putpixel(vbe_data: &VbeModeInfo, color: u8, x: isize, y: isize) {
-    // Pixel FMT = framebuffer + (y * bytes_per_scan_line) + (x * bytes_per_pixel)
+    // Pixel FMT = framebuffer + (y * bytes_per_scan_line) + (x * (bytes_per_pixel / 8))
     let framebuffer = vbe_data.framebuffer as *mut u8;
-    let offset = y * (vbe_data.pitch as isize) + x * (vbe_data.bpp as isize); // framebuffer_base + (y * bytes_per_scanline) + (x * bytes_per_pixel)
+    let offset = y * (vbe_data.pitch as isize) + x * ((vbe_data.bpp as isize) / 8); // framebuffer_base + (y * bytes_per_scanline) + (x * (bytes_per_pixel / 8))
     unsafe {
         *framebuffer.offset(offset as isize) = color;
     }
@@ -72,33 +72,6 @@ fn drawchar(chr: char, x: isize, y: isize, fgcolor: u8, bgcolor: u8, vbe_data: &
         }
     }
 }
-// fn drawchar(chr: char, x: isize, y: isize, fgcolor: u8, bgcolor: u8, vbe_data: &VbeModeInfo) {
-//     unsafe {
-//         let c: u8 = chr as u8;
-//         let font = &F_DATA as *const u8;
-
-//         // Calculate the offset of the glyph data for the given character
-//         let glyph_size: isize = ((F_WIDTH as isize) * (F_HEIGHT as isize)) / (8 as isize); // Number of bytes per glyph
-//         let glyph_offset: isize = (c as isize) * glyph_size; // Offset of the glyph data for this character
-//         let glyph = *font.offset(glyph_offset as isize) as *const u8;
-
-//         // Iterate over each pixel in the glyph data and draw it to the screen
-//         for cy in 0..F_HEIGHT {
-//             putpixel(vbe_data, 0x2, x, y);
-//             for cx in 0..F_WIDTH {
-//                 // let color = if (*glyph.offset(cx as isize) & (0x80 >> cx)) != 0x00 {
-//                 //     fgcolor
-//                 // } else {
-//                 //     bgcolor
-//                 // };
-//                 // putpixel(vbe_data, color, x + cx, y + cy);
-//                 putpixel(vbe_data, 0x2, x + cx, y + cy);
-
-//             }
-//         }
-
-//     }
-// }
 
 // This function is called on panic.
 #[panic_handler]
@@ -113,23 +86,33 @@ pub unsafe extern "C" fn _rust(vbe_mode_info: *const VbeModeInfo) -> ! {
     // Works
     /****************************************** */
     let vbe_data = &*vbe_mode_info;
+    putpixel(vbe_data, 0x02, 1, 1);
+    putpixel(vbe_data, 0x02, 2, 1);
+    putpixel(vbe_data, 0x02, 3, 1);
+    putpixel(vbe_data, 0x02, 4, 1);
+    putpixel(vbe_data, 0x02, 5, 1);
+    putpixel(vbe_data, 0x02, 6, 1);
+    putpixel(vbe_data, 0x02, 7, 1);
+    putpixel(vbe_data, 0x02, 8, 1);
     putpixel(vbe_data, 0x02, 9, 1);
     putpixel(vbe_data, 0x02, 10, 1);
     putpixel(vbe_data, 0x02, 10, 2);
-    // putpixel(vbe_data, 0x02, 10, 3);
-    // putpixel(vbe_data, 0x02, 10, 4);
-    // putpixel(vbe_data, 0x02, 10, 5);
-    // putpixel(vbe_data, 0x02, 10, 6);
-    // putpixel(vbe_data, 0x02, 10, 7);
-    // putpixel(vbe_data, 0x02, 10, 8);
-    // putpixel(vbe_data, 0x02, 10, 9);
-    // putpixel(vbe_data, 0x02, 10, 10);
-    // putpixel(vbe_data, 0x02, 10, 11);
-    // putpixel(vbe_data, 0x02, 10, 12);
-    // putpixel(vbe_data, 0x02, 10, 13);
-    // putpixel(vbe_data, 0x02, 10, 14);
-    // putpixel(vbe_data, 0x02, 10, 15);
-    drawchar('A', 30, 30, 0x2, 0x2, vbe_data);
+    putpixel(vbe_data, 0x02, 10, 3);
+    putpixel(vbe_data, 0x02, 10, 4);
+    putpixel(vbe_data, 0x02, 10, 5);
+    putpixel(vbe_data, 0x02, 10, 6);
+    putpixel(vbe_data, 0x02, 10, 7);
+    putpixel(vbe_data, 0x02, 10, 8);
+    putpixel(vbe_data, 0x02, 10, 9);
+    putpixel(vbe_data, 0x02, 10, 10);
+    putpixel(vbe_data, 0x02, 10, 11);
+    putpixel(vbe_data, 0x02, 10, 12);
+    putpixel(vbe_data, 0x02, 10, 13);
+    putpixel(vbe_data, 0x02, 10, 14);
+    putpixel(vbe_data, 0x02, 10, 15);
+    drawchar('A', 30, 30, 0x0a, 0x00, vbe_data);
+    drawchar('B', 38, 30, 0x0a, 0x00, vbe_data);
+    drawchar('C', 46, 30, 0x0a, 0x00, vbe_data);
 
     // END
     loop {
