62cc12c88c5001aeca44e705f64fde43bdb5bebc
AlexDev404 <34188635+AlexDev404@users.noreply.github.com>
2025-08-01 15:55:29 -0600

    refactor: simplify graphics and text printing headers by removing unnecessary extern "C" blocks and using pragma once

--- src/graphics/graphics.hpp
+++ src/graphics/graphics.hpp
@@ -33,38 +33,29 @@
 #define COLOR_LIGHT_MAGENTA RGB(255, 85, 255)
 #define COLOR_WHITE RGB(255, 255, 255)
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
+// Draw a single pixel to the framebuffer
+void putpixel(int x, int y, unsigned long color);
 
-	// Draw a single pixel to the framebuffer
-	void putpixel(int x, int y, unsigned long color);
+// (overload) Draw a single pixel to the framebuffer with VbeModeInfo
+void putpixel(int x, int y, unsigned long color, const VbeModeInfo *vbe_data);
 
-	// (overload) Draw a single pixel to the framebuffer with VbeModeInfo
-	void putpixel(int x, int y, unsigned long color, const VbeModeInfo *vbe_data);
+// Draw a horizontal line
+void hline(int x1, int x2, int y, unsigned long color);
 
-	// Draw a horizontal line
-	void hline(int x1, int x2, int y, unsigned long color);
+// Draw a vertical line
+void vline(int x, int y1, int y2, unsigned long color);
 
-	// Draw a vertical line
-	void vline(int x, int y1, int y2, unsigned long color);
+// Draw a generic line
+void line(int x1, int y1, int x2, int y2, unsigned long color);
 
-	// Draw a generic line
-	void line(int x1, int y1, int x2, int y2, unsigned long color);
+// Draw a rectangle outline
+void rect(int x1, int y1, int x2, int y2, unsigned long color);
 
-	// Draw a rectangle outline
-	void rect(int x1, int y1, int x2, int y2, unsigned long color);
+// Draw a filled rectangle
+void fillrect(int x1, int y1, int x2, int y2, unsigned long color);
 
-	// Draw a filled rectangle
-	void fillrect(int x1, int y1, int x2, int y2, unsigned long color);
-
-	// Fill the entire screen with a color
-	void fill_screen(unsigned long color);
-
-#ifdef __cplusplus
-}
-#endif
+// Fill the entire screen with a color
+void fill_screen(unsigned long color);
 
 // [BINDINGS]: From stage2 bootloader
 extern "C" const VbeModeInfo *vbe_block();
