a64b31803b061d83550a42bb19f9ee180d9cbbf3
AlexDev404 <34188635+AlexDev404@users.noreply.github.com>
2025-08-04 16:51:24 -0600

    feat: Implement shift key functionality

--- src/devices/init/init.cpp
+++ src/devices/init/init.cpp
@@ -89,6 +89,8 @@ extern "C" void timer_interrupt_c(void)
 	lapic_send_eoi();
 }
 
+static volatile uint32_t column = 0;
+static volatile uint32_t row	= 0;
 extern "C" void isr33_c(void)
 {
 	// Keyboard interrupt handler (vector 0x21)
@@ -97,7 +99,22 @@ extern "C" void isr33_c(void)
 	char scan_code = inportb(0x60); // Read scan code from keyboard controller
 	const char scan_code_chr = scancode_to_char(scan_code);
 	const char scan_code_str[] = {scan_code_chr, '\0'}; // Convert scan code to string for debug output
+	
+	// Print it out
 	send_debug(scan_code_str);
+	print_at(scan_code_str, COLOR_YELLOW, COLOR_BLACK, column, row);
+
+	// Update column and row for next character
+	column++;
+	if (column >= 85) // Assuming 80 columns for the console
+	{
+		column = 0;
+		row++;
+		if (row >= 25) // Assuming 25 rows for the console
+		{
+			row = 0; // Wrap around to the top
+		}
+	}
 	lapic_send_eoi(); // Acknowledge the interrupt
 }
 
