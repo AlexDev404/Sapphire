307ead5e50de7341861539d17777979e22de42e5
AlexDev404 <34188635+AlexDev404@users.noreply.github.com>
2026-05-03 11:44:27 -0600

    fix: bad include

--- src/include/driver/fb/fb.hpp
+++ src/include/driver/fb/fb.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include <arch/noarch/fb/fb.h>
-#include <arch/noarch/fb/constants/color.hpp>
+#include <kernel/graphics/color.hpp>
 
 // Draw a single pixel to the framebuffer
 void putpixel(int x, int y, uint32_t color);
