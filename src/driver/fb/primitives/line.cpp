c41d8b1d8d76ea03b8bbdab81f9323742fc300b3
AlexDev404 <34188635+AlexDev404@users.noreply.github.com>
2026-05-03 11:47:42 -0600

    fix: include errors in line.cpp

--- src/driver/fb/primitives/line.cpp
+++ src/driver/fb/primitives/line.cpp
@@ -1,11 +1,12 @@
 #include <driver/fb/primitives/line.hpp>
 #include <driver/fb/fb.hpp>
-#include <cmath>
+
+static inline int iabs(int x) { return x < 0 ? -x : x; }
 
 // Draw a generic line
 void line(int x1, int y1, int x2, int y2, uint32_t color) {
-    int dx =  abs(x2 - x1), sx = x1 < x2 ? 1 : -1;
-    int dy = -abs(y2 - y1), sy = y1 < y2 ? 1 : -1;
+    int dx =  iabs(x2 - x1), sx = x1 < x2 ? 1 : -1;
+    int dy = -iabs(y2 - y1), sy = y1 < y2 ? 1 : -1;
     int err = dx + dy;
     while (true) {
         putpixel(x1, y1, color);
